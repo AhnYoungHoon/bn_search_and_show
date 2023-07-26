29ddf70d09bcda4cc0af36cb73026cc41997b2da
AhnYoungHoon <dksdudgns312@naver.com>
2023-07-26 15:35:18 +0900

    signup complete

---
+++ src/main/java/com/example/NTSBusinessNum/User/UserCreateForm.java
@@ -0,0 +1,26 @@
+package com.example.NTSBusinessNum.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class UserCreateForm {
+    @Size(min = 3, max = 25)
+    @NotEmpty(message = "사 용 자ID는 필 수 항 목 입 니 다.")
+    private String username;
+
+    @NotEmpty(message = "비 밀 번 호 는 필 수 항 목 입 니 다.")
+    private String password1;
+
+    @NotEmpty(message = "비 밀 번 호 확 인 은 필 수 항 목 입 니 다.")
+    private String password2;
+
+    @NotEmpty(message = "이 메 일 은 필 수 항 목 입 니 다.")
+    @Email
+    private String email;
+}
